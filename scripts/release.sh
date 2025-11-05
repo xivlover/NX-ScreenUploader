11592d1c2b68bf5c7ada982878f595d8c05096f2
Sakari <20642596+sakarie9@users.noreply.github.com>
2025-11-05 17:07:03 +0800

    fix: release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,8 @@ mkdir release
 cd release
 mkdir -p config/sys-screen-capture-uploader
 mkdir -p atmosphere/contents/0100000000000333/flags
-cp ../config.ini config/sys-screen-capture-uploader/config.ini
-cp ../toolbox.json atmosphere/contents/0100000000000333/toolbox.json
-cp ../build/sys-screen-capture-uploader.nsp atmosphere/contents/0100000000000333/exefs.nsp
+cp ../../config.ini config/sys-screen-capture-uploader/config.ini
+cp ../../toolbox.json atmosphere/contents/0100000000000333/toolbox.json
+cp ../../build/sys-screen-capture-uploader.nsp atmosphere/contents/0100000000000333/exefs.nsp
 touch atmosphere/contents/0100000000000333/flags/boot2.flag
-zip -r sys-screen-capture-uploader *
+zip -r sys-screen-capture-uploader ./*
