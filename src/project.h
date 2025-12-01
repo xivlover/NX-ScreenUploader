cdecea56ff9a9afcb881e4a916979a9991abe65a
Sakari <20642596+sakarie9@users.noreply.github.com>
2025-12-01 22:56:58 +0800

    chore: update version to 0.1.6

--- src/project.h
+++ src/project.h
@@ -3,9 +3,9 @@
 
 #define APP_TITLE "NX-ScreenUploader"
 #define APP_AUTHOR "Sakari"
-#define APP_VERSION  "0.1.5"
+#define APP_VERSION  "0.1.6"
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "1"
-#define VERSION_MICRO "5"
+#define VERSION_MICRO "6"
 
 #endif // INCLUDE_GUARD
